5715892c03df6cfc06813d583e088d6518bb9b97
gens <165239315+gensart-x@users.noreply.github.com>
2025-10-15 14:03:42 +0700

    Create users.py

---
+++ app/v1/models/users.py
@@ -0,0 +1,7 @@
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    age: int | None = Field(default=None, index=True)
+    address: str
